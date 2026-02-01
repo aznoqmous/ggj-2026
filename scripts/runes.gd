6bb25e8ee232c64e4f2b5606c9de3978876a123b
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-02-01 14:29:22 +0100

    runes script attribute a rune str by MaskResource

--- scripts/runes.gd
+++ scripts/runes.gd
@@ -3,24 +3,41 @@ class_name Runes
 
 class levelMaskResourceHistory:
 	var array : Array[MaskResource]
-var historyDictionnary : Dictionary[String, levelMaskResourceHistory]
+var _level_history_dictionnary : Dictionary[String, levelMaskResourceHistory]
+
+@export var _mask_default_rune : String = '?'
+@export var _mask_rune_dictionnary : Dictionary[MaskResource, String] = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	historyDictionnary = {}
+	_level_history_dictionnary = {}
+
+# rune generation
+func get_mask_rune(mask : MaskResource) -> String:
+	if (!_mask_rune_dictionnary.has(mask)):
+		return _mask_default_rune
+	return _mask_rune_dictionnary[mask]
+
+func get_masks_rune(masks : Array[MaskResource]) -> String:
+	var runes : String = ""
+	for mask in masks:
+		runes += get_mask_rune(mask)
+	return runes
+
+# history manipulation
 
 func set_history(levelId : String, history : Array[MaskResource]) -> void:
 	var levelHistory = levelMaskResourceHistory.new()
 	levelHistory.array = history
-	historyDictionnary[levelId] = levelHistory
+	_level_history_dictionnary[levelId] = levelHistory
 
 func add_mask_to_history(levelId : String, mask : MaskResource) -> void:
-	var levelHistory = historyDictionnary[levelId]
+	var levelHistory = _level_history_dictionnary[levelId]
 	levelHistory.array.append(mask)
-	historyDictionnary[levelId] = levelHistory
+	_level_history_dictionnary[levelId] = levelHistory
 
 func get_history_object(levelId : String) -> levelMaskResourceHistory:
-	return historyDictionnary[levelId]
+	return _level_history_dictionnary[levelId]
 	
 func get_history_parsed(levelId : String) -> String:
-	return "TODO"
+	return get_masks_rune(get_history_object(levelId).array)
